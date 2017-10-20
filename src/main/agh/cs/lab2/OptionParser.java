67e8477619d8c7b23655fba465574627178485f3
Buś <busiu69@gmail.com>
2017-10-20 19:11:22 +0200

    update z zajec

---
+++ src/main/agh/cs/lab2/OptionParser.java
@@ -0,0 +1,39 @@
+package agh.cs.lab2;
+
+import java.util.*;
+import static agh.cs.lab2.MoveDirection.*;
+
+
+private class OptionParser {
+
+    private ArrayList<MoveDirection> parse(String string[]){
+        ArrayList <MoveDirection> result;
+        result = new ArrayList<MoveDirection>();
+        for(int i = 0; i < string.length; i++){
+            switch(string[i]){
+                case "f":{
+                    result.add(Forward);
+                    break;
+                }
+                case "forward":{
+                    result.add(Forward);
+                    break;
+                }
+                case "b":{
+                    result.add(Backward);
+                    break;
+                }
+                case "backward":{
+                    result.add(Backward);
+                    break;
+                }
+                default: continue;
+            }
+        }
+
+        return result;
+    }
+
+
+
+}
